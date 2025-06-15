5656dd69366c3b457028fa3607cec195b8f2207a
Natalia Stadnik <nata.stadnik.95@inbox.ru>
2025-06-15 21:53:43 +0300

    add settings files

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite'
+import {resolve} from 'path'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
